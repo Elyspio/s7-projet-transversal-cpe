3b322b1aa55b60f7d39c64bc0dfbb0f8fd2972d4
Elyspio <jona.guich69@gmail.com>
2021-01-04 12:03:27 +0100

    Move folders

--- lib/microbit-shared-link/share.ts
+++ lib/microbit-shared-link/share.ts
@@ -4,14 +4,13 @@ const path = require("path")
 const src = path.resolve(__dirname, "src");
 
 const dests = {
-    emergency: path.resolve(__dirname, "..", "microbit-emergency-link", "src", "shared"),
-    simulator: path.resolve(__dirname, "..", "microbit-simulator-link", "src", "shared"),
+    emergency: path.resolve(__dirname, "..", "..", "core", "emergency-manager",  "microbit-emergency-link", "src", "shared"),
+    simulator: path.resolve(__dirname, "..", "..", "core", "simulator", "microbit-simulator-link", "src", "shared"),
 }
 
 async function share(src: string, dest: string) {
     await remove(dest)
     await copy(src, dest, {recursive: true})
-
 }
 
 
